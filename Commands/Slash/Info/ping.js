da1639aea59d206b92edb659f06e262110337099
William Holley <william2sober@gmail.com>
2025-03-07 18:25:37 -0600

    Update ping.js

--- Commands/Slash/Info/ping.js
+++ Commands/Slash/Info/ping.js
@@ -1,6 +1,7 @@
 const { ApplicationCommandType, EmbedBuilder } = require('discord.js');
 const os = require('os');
 require('ms');
+
 module.exports = {
   name: "ping",
   description: "Check bot's ping.",
@@ -17,18 +18,20 @@ module.exports = {
   type: ApplicationCommandType.ChatInput,
   run: async (client, interaction) => {
     try {
-      let days = Math.floor(client.uptime / 86400000)
-      let hours = Math.floor(client.uptime / 3600000) % 24
-      let minutes = Math.floor(client.uptime / 60000) % 60
-      let seconds = Math.floor(client.uptime / 1000) % 60
-      let webLatency = new Date() - interaction.createdAt
-      let apiLatency = client.ws.ping
-      let totalLatency = webLatency + apiLatency
+      let days = Math.floor(client.uptime / 86400000);
+      let hours = Math.floor(client.uptime / 3600000) % 24;
+      let minutes = Math.floor(client.uptime / 60000) % 60;
+      let seconds = Math.floor(client.uptime / 1000) % 60;
+      let webLatency = new Date() - interaction.createdAt;
+      let apiLatency = client.ws.ping;
+      let totalLatency = webLatency + apiLatency;
+
       let emLatency = {
-        Green: '🟢',
-        Yellow: '🟡',
-        Red: '🔴'
-      }
+        Green: '<:Green_Dot:1347724743528415322>',
+        Yellow: '<:Yellow_Dot:1347724793746554961>',
+        Red: '<:Red_Dot:1347724825522606214>'
+      };
+
       interaction.reply({
         embeds: [
           new EmbedBuilder()
@@ -36,22 +39,23 @@ module.exports = {
             .setTitle(`Returns Latency And API Ping`)
             .setFields([
               {
-                name: `📡 Websocket Latency`,
+                name: `<:Satellite:1347725416684851291> Websocket Latency`,
                 value: `>>> \`\`\`yml\n${webLatency <= 200 ? emLatency.Green : webLatency <= 400 ? emLatency.Yellow : emLatency.Red} ${webLatency}ms\`\`\``,
                 inline: true
               },
               {
-                name: `🛰 API Latency`,
+                name: `<:Space_Station:1347725714224582667> API Latency`,
                 value: `>>> \`\`\`yml\n${apiLatency <= 200 ? emLatency.Green : apiLatency <= 400 ? emLatency.Yellow : emLatency.Red} ${apiLatency}ms\`\`\``,
                 inline: true
               },
               {
-                name: `⏲ Uptime`,
+                name: `<:Clock:1347726003375570954> Uptime`,
                 value: `>>> \`\`\`m\n${days} Days : ${hours} Hrs : ${minutes} Mins : ${seconds} Secs\`\`\``,
                 inline: false
               }
-            ])]
-      })
+            ])
+        ]
+      });
     } catch (error) {
       client.slash_err(client, interaction, error);
     }
