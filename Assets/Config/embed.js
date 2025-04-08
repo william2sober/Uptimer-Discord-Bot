d22bc27d3207b3c42aa4131a684fffbf185e8c4c
William Holley <william2sober@gmail.com>
2025-04-08 12:44:33 -0500

    Update embed.js

--- Assets/Config/embed.js
+++ Assets/Config/embed.js
@@ -3,8 +3,8 @@ module.exports = {
   successcolor: "#09b285",
   stanbycolor: "#09b285",
   wrongcolor: "#09b285",
-  footertext: "Powered by William’s Development",
-  footericon: "https://github.com/william2sober/Image-Hosting/blob/main/William%E2%80%99s%20Development%20Images/Uptimer-Icon.png?raw=true",
-  authoricon: "https://github.com/william2sober/Image-Hosting/blob/main/William%E2%80%99s%20Development%20Images/Uptimer-Icon.png?raw=true",
-  loadingicon: "https://github.com/william2sober/Image-Hosting/blob/main/William%E2%80%99s%20Development%20Images/Uptimer-Icon.png?raw=true"
+  footertext: "Powered by Dev",
+  footericon: "",
+  authoricon: "",
+  loadingicon: ""
 }
